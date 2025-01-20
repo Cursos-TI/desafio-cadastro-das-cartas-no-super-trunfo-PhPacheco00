2aba0b5160ae4f521f096b9318d81efda0cf7dd4
PachecoPedro_ <103155457+PhPacheco00@users.noreply.github.com>
2025-01-20 16:06:05 +0000

    Criação das cidades.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,23 @@
         int numPontosTuristicos;
 
         /// Variáveis declaradas!
+
+        printf("Insira o código da cidade: \n");
+        scanf("%i", &codigoCidade);
+        printf("Insira o nome da cidade: \n");
+        scanf("%s", nome);
+        printf("Insira a populacao da cidade: \n");
+        scanf("%f", &populacao);
+        printf("Insira a área da cidade: \n");
+        scanf("%f", &area);
+        printf("Insira o PIB da cidade: \n");
+        scanf("%f", &pib);
+        printf("Insira o numero de pontos turistos: \n");
+        scanf("%i", &numPontosTuristicos);
+        printf("\n");
+        printf("Você criou uma cidade! ");
+
+        /// Criação da cidade.
     
     return 0;
 }
